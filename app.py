4b695ca47163f850321ccfec3ac53d6460d75075
jarb29 <jarb29@gmail.com>
2020-08-03 23:59:13 -0400

    first

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ from flask import Flask, render_template, jsonify, request, redirect, send_from_
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_cors import CORS
-from models import db, Usuario, Tienda, Productos, Factura, Detallefactura 
+from models import db, Payments
 from flask_mail import Mail, Message
 from flask_jwt_extended import (
     JWTManager, jwt_required, create_access_token, get_jwt_identity)
 from flask_bcrypt import Bcrypt
 from werkzeug.utils import secure_filename
+import requests
+
+
+
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static')
-ALLOWED_EXTENSIONS_IMG = {'png', 'jpg', 'jpeg'}
 
 
 app = Flask(__name__)
@@ -21,14 +23,7 @@ app.config['DEBUG'] = True
 app.config['ENV'] = 'development'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'dev.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = 'super-secret-key'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_DEBUG'] = True
-app.config['MAIL_USERNAME'] = 'jarb29@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Amesti2020'
+
 
 
 JWTManager(app)
@@ -41,422 +36,109 @@ mail = Mail(app)
 manager.add_command("db", MigrateCommand)
 
 
-def allowed_file_images(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS_IMG
- 
-def send_mail(subject, sender, recipients, message):
-    msg = Message(subject,
-                  sender=sender,
-                  recipients=[recipients])
-    msg.html = message
-    mail.send(msg)
 
 @app.route('/')
 def root():
     return render_template('index.html')
 
 
-@app.route('/api/loging', methods=['POST'])
-def login():
+@app.route('/api/payments', methods=['POST'])
+def created():
     if not request.is_json:
-        return jsonify({"msg": "Missing JSON in request"}), 400
+        return jsonify({"msg": "Bad request"}), 400
     if request.method == 'POST':
-
-        usuario = request.json.get('email', None)
-        clave = request.json.get('clave', None)
-        if not usuario:
-            return jsonify({"msg": "Falta introducir el email"}), 400
-        if not clave:
-            return jsonify({"msg": "Falta introducir la clave"}), 400
-
-        usua = Usuario.query.filter_by(email = usuario).first()
-
-        if not usua:
-            return jsonify({"msg": "Usuario no existe"}), 404
-
-        if bcrypt.check_password_hash(usua.clave, clave):
-            access_token = create_access_token(identity = usua.nombre)
-            data = {
-                "access_token": access_token,
-                "Usuario": usua.serialize()
-            }
-            return jsonify(data), 200
-        else:
-            return jsonify({"msg": "email/ clave errados favor verificar"}), 401
-
-
-
-@app.route('/api/register', methods=['POST'])
-def register():
-    if not request.is_json:
-        return jsonify({"msg": "Missing JSON in request"}), 400
-
-    usuario = request.json.get('nombre', None)
-    clave = request.json.get('clave', None)
-    apellido = request.json.get('apellido', None)
-    email = request.json.get('email', None)
-    direccion = request.json.get('direccion', None)
-    telefono = request.json.get('telefono', None)
-
-
-    if not usuario:
-        return jsonify({"msg": "Falta el nombre"}), 400
-    if not apellido:
-        return jsonify({"msg": "Falta el apellido"}), 400
-    if not email:
-        return jsonify({"msg": "Falta el email"}), 400
-    usua = Usuario.query.filter_by(email = email).first()
-    if usua:
-        return jsonify({"msg": "Usuario existe por favor elegir diferente Email"}), 400
-    if not direccion:
-        return jsonify({"msg": "Falta la direccion"}), 400
-    if not telefono:
-        return jsonify({"msg": "Falta el telefono"}), 400
-    if not clave:
-        return jsonify({"msg": "Falta la clave"}), 400
-    
-
-    
+        r = requests.get("https://mindicador.cl/api").json()
+        valor_uf = r['uf']['valor']
         
-    usua = Usuario()
-    usua.nombre = usuario
-    usua.clave = bcrypt.generate_password_hash(clave) 
-    usua.apellido = apellido
-    usua.email = email
-    usua.direccion = direccion
-    usua.telefono = telefono
-    db.session.add(usua)
-    db.session.commit()
-    html = render_template('email-registerCliente.html', user=usua)
-    send_mail("Registro", "jarb29@gmail.com", usua.email, html)
+        name = request.json.get('name', None)
+        lastName = request.json.get('lastName', None)
+        description = request.json.get('description', None)
+        serviceHour = request.json.get('serviceHour', None)
+        amountOfService = request.json.get('amountOfService', None)
+        dayAmmountUf = valor_uf 
+
+        if not name:
+            return jsonify({"msg": "Name Not Found"}), 404
+        if not lastName:
+            return jsonify({"msg": "lastName Not Found"}), 400
+        if not description:
+            return jsonify({"msg": "description Not Found"}), 400
+        if not serviceHour:
+            return jsonify({"msg": "serviceHour Not Found"}), 400
+        if not amountOfService:
+            return jsonify({"msg": "amountOfService Not Found"}), 400
+        if not dayAmmountUf:
+            return jsonify({"msg": "dayAmmountUf  Not Found"}), 400
+        
+        usua = Payments()
+        usua.name = name
+        usua.lastName = lastName
+        usua.description = description
+        usua.serviceHour = serviceHour
+        usua.amountOfService = amountOfService
+        usua.dateAmmountUF =dayAmmountUf
+        db.session.add(usua)
+        db.session.commit()
+        return jsonify({"msg": "payment created"}), 201
 
 
+@app.route('/api/payments/<int:id>', methods=['GET', 'DELETE'])
+def get_products(id):
+    if not request.is_json:
+        return jsonify({"msg": "Bad request"}), 400
 
-
-    access_token = create_access_token(identity=usua.nombre)
-   
-     
-    data = {
-        "access_token": access_token,
-        "Usuario": usua.serialize()
-    }
-    return jsonify(data),  200
-   
-
-
-
-@app.route('/api/register/producto', methods=['POST'])
-def producto():
-
-    nombre = request.form.get('nombreProducto', None)
-    description = request.form.get('descripcion', None)
-    stock = request.form.get('stock', None)
-    precio = request.form.get('precio', None)
-    tienda_id = request.form.get('tienda_id', None)
-    categoria = request.form.get('categoria', None)
-    file = request.files['avatar']
-
-  
-    if file:
-        if file.filename == '': 
-            return jsonify({"msg": "Agregar nombre a la foto"}), 400
-    if not nombre or nombre =='':
-        return jsonify({"msg": "Falta el nombre del producto"}), 400
-    if not description or description == '':
-        return jsonify({"msg": "Falta la description "}), 400
-    if not stock or stock == '':
-        return jsonify({"msg": "Falta la cantidad disponible "}), 400
-    if not precio or precio == '':
-        return jsonify({"msg": "Falta el precio"}), 400
-    if not tienda_id:
-        return jsonify({"msg": "Falta el id de la tienda relacionada"}), 400
-    if not categoria or categoria == '':
-        return jsonify({"msg": "Falta la categoria"}), 400
-
-    if file and allowed_file_images(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(os.path.join(app.config['UPLOAD_FOLDER'], 'img/avatars'), filename))
-
-    usua = Productos.query.filter_by(tienda_id=tienda_id, nombre = nombre).first()
-   
-    if usua:
-        return jsonify({"msg": "EL producto ya existe"}), 400
-    usua = Productos()
-    usua.nombre = nombre 
-    usua.stock = stock
-    usua.description = description
-    usua.precio = precio
-    usua.categoria = categoria
-    usua.tienda_id = tienda_id 
-
-    if file:
-        usua.avatar = filename
-
-    db.session.add(usua)
-    db.session.commit()
-    data = {
-        "Producto": usua.serialize()
-    }
-    return jsonify({'msg': 'Producto agregado exitosamente'}), 200
-
-
-
-@app.route('/api/admin/<int:id>', methods=['GET', 'DELETE'])
-@jwt_required
-def productos(id):
     if request.method == 'GET':
-        listaProductos = Productos.query.filter_by(tienda_id=id).all()
+        listaProductos = Payments.query.filter_by(id=id).all()
+        if not listaProductos :
+            return jsonify({"msg": "id Not Found"}), 404
         listaProductos = list(map(lambda listaProductos: listaProductos.serialize(), listaProductos))
         return jsonify(listaProductos), 200
     
     if request.method == 'DELETE':
-        deleteProducto = Productos.query.filter_by(id=id).first()
+        deleteProducto = Payments.query.filter_by(id=id).first()
+        if not deleteProducto:
+            return jsonify({"msg": "id Not Found"}), 404
+
         db.session.delete(deleteProducto)
         db.session.commit()
-
         return jsonify({"msg": "Producto eliminado"}), 200
 
 
-@app.route('/api/editar/producto/<int:id>', methods=['PUT'])
-@jwt_required
-def editarProducto(id):
-    editProducto = Productos.query.get(id)
+@app.route('/api/allPayments', methods=['GET'])
+def get_all():
+    listaPayments = Payments.query.all()
+    if not listaPayments:
+        return jsonify({"msg": "Not Found"}), 404
+    listaPayments = list(map(lambda listaPayments: listaPayments.serialize(), listaPayments))
+    return jsonify(listaPayments), 200
 
-    nombre = request.form.get('nombre', None)
-    description = request.form.get('descripcion', None)
-    stock = request.form.get('stock', None)
-    precio = request.form.get('precio', None)
-    tienda_id = request.form.get('tienda_id', None)
-    categoria = request.form.get('categoria', None)
+
+@app.route('/api/payments/<int:id>', methods=['PUT'])
+def editarPayment(id):
+    if not request.is_json:
+        return jsonify({"msg": "Bad request"}), 400
+    
+    editPayment = Payments.query.get(id)
+    name = request.json.get('name', None)
+    lastName = request.json.get('lastName', None)
+    description = request.json.get('description', None)
+    serviceHour = request.json.get('serviceHour', None)
+    amountOfService = request.json.get('amountOfService', None)
+    dayAmmountUf = request.json.get('dayAmmountUf', None)
     
 
-    if nombre != '':
-        editProducto.nombre = nombre
+    if name != '':
+        editPayment.name = name
     if description !='':
-        editProducto.description = description
-    if stock !='':
-        editProducto.stock = stock
-    if precio != '':
-        editProducto.precio = precio 
-
+        editPayment.description = description
+    if lastName  !='':
+        editPayment.lastName = lastName 
+    if serviceHour !='':
+        editPayment.serviceHour = serviceHour
+    if amountOfService != '':
+        editPayment.amountOfService = amountOfService
     db.session.commit()
-
-    return ({'msg': 'Producto actualizado'})  
-
-
-
-@app.route('/api/tienda/<int:id>', methods=['GET'])
-@jwt_required
-def tiendaSeleccionada(id):
-    listaProductos = Productos.query.filter_by(tienda_id=id).all()
-    listaProductos = list(map(lambda listaProductos: listaProductos.serialize(), listaProductos))
-    return jsonify(listaProductos), 200
-
-
-@app.route('/api/producto/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER'], 'img/avatars'), filename)
-
-
-
-
-
-
-@app.route('/api/tienda/<filename>')
-def uploaded_fil(filename):
-    return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER'], 'img/avatars'), filename)
-
-
-
-
-
-
-
-@app.route('/api/registerTienda', methods=['POST'])
-def registerTienda():
-    if not request.is_json:
-        return jsonify({"msg": "Missing JSON in request"}), 400
-
-    usuario = request.json.get('nombre', None)
-    categoria = request.json.get('categoria', None)
-    rut = request.json.get('rut', None)
-    email = request.json.get('email', None)
-    latitude = request.json.get('latitude', None)
-    longitude = request.json.get('longitude', None)
-    clave = request.json.get('clave', None)
-  
-
-
-    if not usuario:
-        return jsonify({"msg": "Falta el nombre de la Tienda"}), 400
-    if not categoria:
-        return jsonify({"msg": "Falta la categoria de la Tienda"}), 400
-    if not rut:
-        return jsonify({"msg": "Falta el rut de la Tienda"}), 400
-    usua_rut = Tienda.query.filter_by(rut = rut).first()
-    if usua_rut:
-        return jsonify({"msg": "Favor proveer Rut valido"}), 400
-    if not email:
-        return jsonify({"msg": "Falta el Email de la Tienda"}), 400
-    usua = Tienda.query.filter_by(email = email).first()
-    if usua:
-        return jsonify({"msg": "Usuario existe por favor elegir un Email diferente"}), 400
-    if not clave:
-        return jsonify({"msg": "Falta la clave"}), 400
-    if not latitude:
-        return jsonify({"msg": "Falta la latitude de la Tienda"}), 400
-    if not longitude:
-        return jsonify({"msg": "Falta el longitud de la Tienda"}), 400
-    
-        
-    usua = Tienda()
-    usua.nombre = usuario
-    usua.clave = bcrypt.generate_password_hash(clave) 
-    usua.categoria = categoria
-    usua.rut = rut
-    usua.email = email
-    usua.latitude = latitude
-    usua.longitude = longitude
-    db.session.add(usua)
-    db.session.commit()
-    html = render_template('email-registerTienda.html', user=usua)
-    send_mail("Registro", "jarb29@gmail.com", usua.email, html)
-    
-    access_token = create_access_token(identity=usua.nombre)
-     
-    data = {
-        "access_token": access_token,
-        "Tienda": usua.serialize()
-    }
-    return jsonify(data),  200
-
-
-
-
-@app.route('/api/logingTienda', methods=['POST'])
-def logingTienda():
-    if not request.is_json:
-        return jsonify({"msg": "Missing JSON in request"}), 400
-    if request.method == 'POST':
-
-        usuario = request.json.get('email', None)
-        clave = request.json.get('clave', None)
-        if not usuario:
-            return jsonify({"msg": "Falta introducir el email"}), 400
-        if not clave:
-            return jsonify({"msg": "Falta introducir la clave"}), 400
-
-        usua = Tienda.query.filter_by(email = usuario).first()
-
-        if not usua:
-            return jsonify({"msg": "Usuario no existe"}), 404
-
-        if bcrypt.check_password_hash(usua.clave, clave):
-            access_token = create_access_token(identity = usua.nombre)
-            data = {
-                "access_token": access_token,
-                "Tienda": usua.serialize()
-            }
-            return jsonify(data), 200
-        else:
-            return jsonify({"msg": "email/ clave errados favor verificar"}), 401
-
-
-
-@app.route('/api/mapa', methods=['GET'])
-@jwt_required
-def protected():
-
-    datosProductos = Tienda.query.all()
-    datosProductos = list(map(lambda datosProductos: datosProductos.serialize(), datosProductos))
-    
-    return jsonify(datosProductos), 200
-
-
-
-
-
-
-@app.route('/api/checkout/<int:id>', methods=['PUT'])
-def checkout(id):
-    if not request.is_json:
-        return jsonify({"msg": "Missing JSON in request"}), 400
-
-    ItemCompradoId= request.json.get('ItemProductoCompradoId', None)
-    CantidaProductoComprado = request.json.get('CantidaProductoComprado', None)
-    precioProductoSeleccionado = request.json.get('precioProductoSeleccionado', None)
-    usuario_id = request.json.get('usuario_id', None)
-    totalFactura = request.json.get('totalFactura', None)
-    totalProductosComprados = request.json.get('totalProductosComprados', None)
-    usuarioActual = request.json.get('usuarioActual', None)
-    emailTiendaSeleccionada = request.json.get('emailTiendaSeleccionada', None)
-
- 
-    email = Usuario.query.filter_by(id = usuario_id).first().email
-    productos = Productos.query.filter(Productos.id.in_(ItemCompradoId)).all()
-
-    
- 
-    usua = Factura()
-    usua.usuario_factura_id = usuario_id
-    usua.total = totalFactura
-    db.session.add(usua)
-    db.session.commit()
-       
-
-
-
-    factura_id = Factura.query.order_by(Factura.id.desc()).first().id
-    print(factura_id)
-    i=0
-    for prod in productos:
-        usua = Detallefactura()
-        usua.productos_comprados = int(CantidaProductoComprado[i])
-        usua.factura_id= factura_id
-        usua.producto_id = int(ItemCompradoId[i])
-        usua.precio = int(precioProductoSeleccionado[i])
-        db.session.add(usua)
-        db.session.commit()
-        i=i+1
-
-
-    i=0
-    for prod in productos:
-     
-        prod.stock = int(prod.stock) - int(CantidaProductoComprado[i])
-        i=i+1
-        db.session.commit()
-    
-    html = render_template('email-compraProductos.html', users=totalProductosComprados)
-    send_mail("Compra", "jarb29@gmail.com", email, html)
-    html = render_template('email-ProductosComprados.html', usuarioactual = usuarioActual, users=totalProductosComprados)
-    send_mail("Productos comprados", "jarb29@gmail.com", emailTiendaSeleccionada, html)
-    
-
-    datosProductos = Productos.query.filter_by(tienda_id = id).all()
-    datosProductos = list(map(lambda datosProductos: datosProductos.serialize(), datosProductos))
-    
-    
-    return jsonify(datosProductos), 200
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return jsonify({"msg": "Payment update succesfuly"}), 200
 
 
 
--- models.py
+++ models.py
@@ -13,127 +13,28 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
 
-class Usuario(db.Model):
-    __tablename__ = 'usuario'
+class Payments(db.Model):
+    __tablename__ = 'payments'
     id = db.Column(db.Integer, primary_key = True)
-    nombre = db.Column(db.String(100), nullable = False)
-    apellido = db.Column(db.String(100), nullable = False)
-    email = db.Column(db.String(100), nullable = False, unique=True)
-    direccion = db.Column(db.String(100), nullable = False)
-    telefono = db.Column(db.String(100), nullable = False)
-    clave = db.Column(db.String(100), nullable = True)
-    usuario_factura = db.relationship('Factura',  backref= 'detale_factura', lazy = True)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    name = db.Column(db.String(100), nullable = False)
+    lastName = db.Column(db.String(100), nullable = False)
+    description = db.Column(db.String(100), nullable = False, unique=True)
+    serviceHour = db.Column(db.Integer, nullable = False)
+    amountOfService = db.Column(db.Integer, nullable = False)
+    dateAmmountUF = db.Column(db.Integer, nullable = False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return f"usuario('{self.nombre}', '{self.apellido}', '{self.email}', '{self.direccion}', '{self.telefono}')"
+        return f"usuario('{self.name}', '{self.lastName}', '{self.description}', '{self.serviceHour}', '{self.amountOfService}', '{self.amountOfService}', '{self.dateAmmountUF}')"
 
     def serialize(self):
         return {
             "id":self.id,
-            "nombre": self.nombre,
-            "apellido": self.apellido,
-            "email": self.email,
-            "direccion": self.direccion,
-            "telefono":self.telefono,
+            "name": self.name,
+            "lastName": self.lastName,
+            "description": self.description,
+            "serviceHour": self.serviceHour,
+            "amountOfService":self.amountOfService,
+            "dateAmmountUF":self.dateAmmountUF,
+            "date":self.date,
         }  
-
-
-
-class Tienda(db.Model):
-    __tablename__ = 'tienda'
-    id = Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable = False)
-    categoria = db.Column(db.String(100), nullable = False)
-    rut = db.Column(db.String(100), nullable = False, unique=True)
-    email = db.Column(db.String(120), nullable = False, unique=True)
-    latitude = db.Column(db.String(100), nullable = False, unique=True)
-    longitude = db.Column(db.String(100), nullable = False, unique=True)
-    clave = db.Column(db.String(100), nullable = False)
-    productos_tienda = db.relationship('Productos', backref= 'productos_t', lazy = True)
-  
-    
-    def __repr__(self):
-        return f"Tienda('{self.nombre}', '{self.rut}', '{self.email}', '{self.latitude}', '{self.longitude}', '{self.categoria}', '{self.clave}')"
-
-    def serialize(self):
-        return {
-            "id":self.id,
-            "nombre": self.nombre,
-            "rut": self.rut,
-            "email": self.email,
-            "latitude": self.latitude,
-            "longitude": self.longitude,
-            "categoria": self.categoria,
-        }  
-
-
-
-class Productos(db.Model):
-    __tablename__ = 'productos'
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable = False)
-    avatar = db.Column(db.String(100), nullable = False, default = 'favicon.ico')
-    stock = db.Column(db.String(100), nullable = False)
-    precio = db.Column(db.String(100), nullable = False)
-    description = db.Column(db.String(100), nullable = False)
-    categoria = db.Column(db.String(100), nullable = False)
-    tienda_id = db.Column(db.Integer, db.ForeignKey('tienda.id'), nullable=False)
-    factura_Productos = db.relationship('Detallefactura', backref= 'factura_p', lazy = True)
-
-    def __repr__(self):
-        return f"Productos('{self.nombre}', '{self.avatar}', '{self.stock}', '{self.precio}')"
-
-    def serialize(self):
-        return {
-            "id":self.id,
-            "nombre": self.nombre,
-            "avatar": self.avatar,
-            "stock": self.stock,
-            "precio": self.precio,
-            "categoria":self.categoria,
-            "description":self.description,
-        }  
-
-
-class Factura(db.Model):
-    __tablename__ = 'factura'
-    id = db.Column(db.Integer, primary_key=True)
-    usuario_factura_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
-    factura_detalle = db.relationship('Detallefactura',  backref= 'detalle', lazy = True)
-    total = db.Column(db.String(100), nullable = False)
-
-    def __repr__(self):
-        return f"{self.id}"
-
-    def serialize(self):
-        return {
-            "id": self.id,
-             "usuario": self.usuario,
-             "factura_detalle": self.factura_detalle,
-             "total": self.total,
-        }  
-
-
-class Detallefactura(db.Model):
-    __tablename__ = 'detallefactura'
-    id = db.Column(db.Integer, primary_key=True)
-    productos_comprados = db.Column(db.String(100), nullable = False)
-    factura_id = db.Column(db.String(100), db.ForeignKey('factura.id'))
-    producto_id = db.Column(db.Integer, db.ForeignKey('productos.id'), nullable=False)
-    precio = db.Column(db.String(100), nullable = False)
-
-    def __repr__(self):
-        return f"Detallefactura('{self.id}')"
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "productos_comprados": self.productos_comprados,
-            "factura_id": self.factura_id,
-            "producto_id": self.producto_id ,
-            "precio:": self.precio,
-        }  
-
-
-
